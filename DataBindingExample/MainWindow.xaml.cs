d1325fb4e46d50fb1df8f5a1f1f1a4e7697bc4aa
pedrolopez687 <19470410@itcampeche.edu.mx>
2020-11-05 04:29:08 -0600

    Versión inicia del proyecto. Utiliza manejo de eventos para funcionar.

---
+++ DataBindingExample/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Evento evento;
+        public MainWindow()
+        {
+            InitializeComponent();
+            evento = new Evento()
+            {
+                Title = "Reunión de trabajo",
+                Date = new DateTime(2020, 10, 23)
+            };
+            txtbxnombre.Text = evento.Title;
+            datepickfecha.SelectedDate = evento.Date;
+
+        }
+
+        private void btnmodificar_Click(object sender, RoutedEventArgs e)
+        {
+            evento.Title = evento.Title.ToLower();
+            evento.Date = evento.Date.AddDays(1);
+            txtbxnombre.Text = evento.Title;
+            datepickfecha.SelectedDate = evento.Date;
+            MessageBox.Show(evento.Title + "\n" + evento.Date);
+        }
+    }
+}
